eb3e9394c4561debf7e73c7f4547366d8c3f0f31
Shandra <shandrai@gmail.com>
2016-10-12 16:07:06 -0700

    Set up mLab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/whisky-app' :
+                            'mongodb://default:default@ds029793.mlab.com:29793/whisky-app' :
                             'mongodb://localhost/whisky-app-dev');
 exports.PORT = process.env.PORT || 8080;
